95f4d342bb84b92e966fd25a62bfcfeef3cc54d7
mdriazuddinriad5 <mdriazuddinriad5@gmail.com>
2023-11-08 02:08:19 +0600

    Firebase setup

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -3,6 +3,7 @@ import MainLayout from "../layout/MainLayout";
 import Home from "../pages/Home/Home";
 import Rooms from "../pages/Rooms/Rooms";
 import RoomDetails from "../pages/Rooms/RoomDetails";
+import Bookings from "../pages/Bookings/Bookings";
 
 
 
@@ -23,6 +24,10 @@ const Router = createBrowserRouter([
                 path: '/roomDetails/:id',
                 element: <RoomDetails></RoomDetails>,
                 loader: ({ params }) => fetch(`http://localhost:5000/rooms/${params.id}`)
+            },
+            {
+                path: '/bookings',
+                element: <Bookings></Bookings>
             }
         ])
     }
